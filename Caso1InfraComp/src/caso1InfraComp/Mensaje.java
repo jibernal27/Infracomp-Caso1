538739b17e020cfc2db284baf92a2d38edd1b448
juanpa10 <jp.camacho10@uniandes.edu.co>
2016-02-23 10:54:51 -0500

    Mensaje

--- Caso1InfraComp/src/caso1InfraComp/Mensaje.java
+++ Caso1InfraComp/src/caso1InfraComp/Mensaje.java
@@ -2,4 +2,25 @@ package caso1InfraComp;
 
 public class Mensaje {
 
+	private Integer consulta;
+	
+	private Integer respuesta;
+	
+	public Mensaje(){
+		consulta = null;
+		respuesta = null;
+	}
+	
+	public void crearConsulta (Integer nConsulta){
+		this.consulta = nConsulta;
+	}
+	
+	public void crearREspuesta(Integer nRespuesta){
+		this.respuesta = nRespuesta;
+	}
+	
+	public Integer darRespuesta(){
+		return respuesta;
+	}
+	
 }
